4e148bc983988160f6e03748acdd7c343daf997c
jhon-sdsharp <Jhon.A@sdsharp.com>
2024-04-23 01:44:55 -0500

    feat: context implementation for the app, cardlist implementation and a smallfix over header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { DeviceProvider } from './contexts/DeviceContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <DeviceProvider>
+      <App />
+    </DeviceProvider>
   </React.StrictMode>,
 )
