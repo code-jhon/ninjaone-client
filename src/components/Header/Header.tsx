055238f81609628aa70c01403d19e890fc0980c3
jhon-sdsharp <Jhon.A@sdsharp.com>
2024-04-22 23:41:38 -0500

    style: styling features for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Logo from '../../assets/logo.svg';
+
+const HeaderContainer = styled.header`
+  display: flex;
+  background: #002A42;
+  height: 50px;
+  ;
+`;
+
+const Image = styled.img`
+  width: 120px;
+  margin-left: 24px;
+`;
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <img src={Logo} alt="Image" className="left-aligned h-50" />
-    </header>
+    <HeaderContainer>
+      <Image src={Logo} alt="Image" />
+    </HeaderContainer>
   );
 };
 
